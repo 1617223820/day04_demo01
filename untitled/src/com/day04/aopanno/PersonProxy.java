6e68a1fcb9ce601c068cf0ba98b7581a84b5f753
yang <1617223820@qq.com>
2023-01-04 17:17:07 +0800

    bug2_update01

---
+++ untitled/src/com/day04/aopanno/PersonProxy.java
@@ -0,0 +1,16 @@
+package com.day04.aopanno;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Order(0)//优先级设置，数字越低，优先级越高
+public class PersonProxy {
+    @Before(value = "execution(* com.day04.aopanno.User.add(..))")
+    public void personAdd(){
+        System.out.println("这是另一个增强类的前置通知");
+    }
+}
--- untitled/src/com/day04/aopanno/UserProxy.java
+++ untitled/src/com/day04/aopanno/UserProxy.java
@@ -2,38 +2,46 @@ package com.day04.aopanno;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 
 @Component//创建bean实例
 @Aspect//生成代理对象
+@Order(2)
 public class UserProxy {
+
+    @Pointcut(value = "execution(* com.day04.aopanno.User.add(..))")//切入点抽取
+    public void point_demo(){
+    }
+
+
     //前置通知
-    @Before(value = "execution(* com.day04.aopanno.User.add(..))")
+    @Before(value = "point_demo()")
     public void before(){
         System.out.println("前置通知：Before");
     }
 
     //后置通知
-    @AfterReturning(value = "execution(* com.day04.aopanno.User.add(..))")
+    @AfterReturning(value = "point_demo()")
     public void afterReturning(){
         System.out.println("后置通知：AfterReturning");
     }
 
     //最终通知
-    @After(value = "execution(* com.day04.aopanno.User.add(..))")
+    @After(value = "point_demo()")
     public void after(){
         System.out.println("最终通知：After");
     }
 
     //异常通知
-    @AfterThrowing(value = "execution(* com.day04.aopanno.User.add(..))")
+    @AfterThrowing(value = "point_demo()")
     public void afterThrowing(){
         System.out.println("异常通知：AfterThrowing");
     }
 
     //环绕通知
-    @Around(value = "execution(* com.day04.aopanno.User.add(..))")
+    @Around(value = "point_demo()")
     public void Around(ProceedingJoinPoint proceedingJoinPoint)throws Throwable{
         System.out.println("环绕之前....");
         //被增强的方法执行
